afce881147dc031c9f462dc2c0d888735c699594
sardor <sultonov.sardor19@gmail.com>
2022-10-01 11:50:08 +0500

    js homework lesson 1

---
+++ index.js
@@ -0,0 +1,260 @@
+// 1dars uy ishi Tiplar
+
+// 1-misol
+
+// let L = +prompt("uzunlikni kiriting");
+// let m = 1000;
+
+// L*=m
+
+// console.log(`${L} metrga teng`)
+
+// 2-misol
+
+// let M = +prompt('kilogramni kiriting');
+// let t = 1000;
+
+// M/=t
+
+// console.log(`${M} tonna`)
+
+// 3-misol
+
+// 4-misol
+
+// 5-misol
+
+// let son1 = +prompt(`son kiriting`);
+// let son2 = +prompt(`son kiriting`)
+
+// son1 += son2
+
+// console.log(son1)
+
+// 6-misol
+
+// let son1 = +prompt(`son kiriting`);
+// let son2 = +prompt(`son kiriting`)
+
+// son1 *= son2
+
+// console.log(son1)
+
+// 7-misol
+
+// 8-misol
+
+// let n = +prompt('secundni kiriting');
+
+// m = n / 60
+// s = m / 60
+
+// console.log(m , s);
+
+// Mantiqiy amallar if,else
+
+// 1-misol
+
+// let son = +prompt(`son kiriting`);
+
+// if (son >= 0 ){
+//   console.log("Musbat")
+// }else{
+//   console.log("Manfiy")
+// }
+
+// 2-misol
+
+// let son = +prompt("son kiriting");
+
+// if (son%2===1){
+//   console.log("toq son")
+// }else{
+//   console.log("juft son")
+// }
+
+// 3-misol
+
+// let son = +prompt("son kiriting");
+
+// if (son%2===0){
+//   console.log("juft son")
+// }else{
+//   console.log("toq son")
+// }
+
+// 4-misol
+
+// let a = +prompt('a= ni kiriting');
+// let b = +prompt('b= ni kiriting');
+
+// if (a>0 && b>0){
+//   console.log("To'g'ri javob")
+// }else{
+//   console.log("Xato javob")
+// }
+
+// 5-misol 
+
+// let a = +prompt("a= kiriting");
+// let b = +prompt("b= kiriting");
+// let c = +prompt("c= kiriting");
+
+// if ( a>b && b>c){
+//   console.log("true")
+// }else{
+//   console.log("false")
+// }
+
+// 6-misol
+
+// let a = +prompt("a= kiriting");
+// let b = +prompt("b= kiriting");
+// let c = +prompt("c= kiriting");
+
+// if (a<b && b<c){
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }
+
+// 7-misol
+
+// let a = +prompt('a= kiriting');
+// let b = +prompt('b= kiriting');
+
+// if (a%2===1 && b%2===1){
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }
+
+// 8-misol
+
+// let a = +prompt('a= kiriting');
+// let b = +prompt('b= kiriting');
+// let c = +prompt('c= kiriting');
+// let d = +prompt('d= kiriting');
+
+// if (a,b,c,d>=0){
+//   console.log("True")
+// }else{
+//   console.log("False")
+// }
+
+// 9-misol
+
+// let a = +prompt('a= kiriting');
+// let b = +prompt('b= kiriting');
+// let c = +prompt('c= kiriting');
+
+// if (a,b>0){
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }
+
+// 10-misol
+
+// let haftaKuni = +prompt("Hafta kunini kiriting");
+
+// if (haftaKuni===1){
+//   console.log('Dushanba')
+// }else if (haftaKuni===2){
+//   console.log('Sheshanba')
+// }else if (haftaKuni===3){
+//   console.log('Chorshanba')
+// }else if (haftaKuni===4){
+//   console.log('Payshanba')
+// }else if (haftaKuni===5){
+//   console.log('Juma')
+// }else if (haftaKuni===6){
+//   console.log('Shanba')
+// }else if (haftaKuni===7){
+//   console.log('Yakshanba')
+// }else{
+//   console.log("Bunday kunn yo'q")
+// }
+
+// 11-misol
+
+// let telNomer = +prompt('nomer kiriting');
+
+// if (telNomer===99){
+//   console.log('uzmobile') 
+// }else if (telNomer===97){
+//   console.log('mobiuz')
+// }else if (telNomer===95){
+//   console.log('uzmobile')
+// }else if (telNomer===94){
+//   console.log('ucel')
+// }else if (telNomer===90){
+//   console.log('biline')
+// }else if (telNomer===33){
+//   console.log('humans')
+// }else{
+//   console.log('bunday nomer mavjud emas')
+// }
+
+// 12-misol
+
+// let son = +prompt('son kiriting');
+
+// if (son >= 0){
+//   son++
+//   console.log(son)
+// }else if (son < 0){
+//   son--
+//   console.log(son)
+// }
+
+// 13-misol
+
+// let son = +prompt('son kiriting');
+
+// if (son >= 0){
+//   son**=3
+//   console.log(son)
+// }else if (son < 0){
+//   son/=2
+//   console.log(son)
+// }
+
+// 14-misol
+
+// let a = +prompt('a=');
+// let b = +prompt('b=');
+
+// if (a > b){
+//   console.log(a + 'a katta')
+// }else{
+//   console.log(b + 'b katta')
+// }
+
+// 15-misol
+
+// let a = +prompt('a=');
+// let b = +prompt('b=');
+// let c = +prompt('c=');
+
+// if (a > b && a > c){
+//   console.log(a + ' a katta')
+// }else if (b > a && b > c){
+//   console.log(b + ' b katta')
+// }else{
+//   console.log(c + ' c katta')
+// }
+
+// 16-misol 
+
+// let a = +prompt('a=');
+// let b = +prompt('b=');
+// let c = +prompt('c=');
+
+// if (a < b && a < c){
+//   console.log(a + ' a kichik')
+// }else if (b < a && b < c){
+//   console.log(b + ' b kichik')
+// }else{
+//   console.log(c + ' c kichik')
+// }
